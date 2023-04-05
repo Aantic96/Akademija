9a5301c89b449e426e3ec70f1d95b02f4f5fe649
Antonela <gemory.mei@gmail.com>
2023-04-05 20:22:22 +0000

    Added insert and group insert method

--- app/Connection.php
+++ app/Connection.php
@@ -55,9 +55,57 @@ class Connection
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function insert(string $tableName, array $addValues)
+    public function insert(string $tableName, array $values): void
     {
-        //
+        $columns = [];
+        $data = [];
+        foreach ($values as $key => $value) {
+            $columns[] = $key;
+            $data[] = $value;
+        }
+
+        $query = "INSERT INTO $tableName(" . implode(', ', $columns) .
+            ") VALUES(" . implode(', ', $data) . ")";
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+    }
+
+    public function groupInsert(string $tableName, array $values): void
+    {
+        if (!$this->isAssoc($values)) {
+            echo "Wrong method";
+            exit;
+        }
+        
+        $columns = [];
+        $data = [];
+        foreach ($values as $value){
+            $data[] = '(';
+            foreach ($value as $k => $v){
+                if(!in_array($columns))
+                {
+                    $columns[] = $k;
+                }
+                $data[] = $v;
+            }
+            $data[] = ')';
+        }
+
+        $query = "INSERT INTO $tableName(" . implode(', ', $columns) .
+            ") VALUES(" . implode(', ', $data) . ")";
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+    }
+
+    public function isAssoc(array $array): bool
+    {
+        if (empty($array)) {
+            return false;
+        }
+        $keys = array_keys($array);
+        return array_keys($keys) !== $keys;
     }
 
     public function update(string $tableName, array $updateValues, array $condition)
